bacbc9397735cb2f84308ef81cb2b044e44970ce
AsPro777 <92028919+AsPro777@users.noreply.github.com>
2021-11-03 10:30:32 +0300

    commit2
    commit2

--- module/User/src/Controller/Factory/WidgetTicketPayControllerFactory.php
+++ module/User/src/Controller/Factory/WidgetTicketPayControllerFactory.php
@@ -5,24 +5,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-namespace User\Controller\Factory;//это прописываем всегда
+namespace User\Controller\Factory;
 
-use Interop\Container\ContainerInterface;//это тоже стандартно
-use Zend\ServiceManager\Factory\FactoryInterface;//и это тоже стандартно
-use User\Service\UserManager;//ниже в $container->get в качестве параметра будем использовать класс из этого пространства имен
-//Поэтому его и подключаем.
-use Application\Service\ImageManager;//это по тому же принципу что и в предыд строке подключаем
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use User\Service\UserManager;
+use Application\Service\ImageManager;
 
-class WidgetTicketPayControllerFactory implements FactoryInterface//это стандартно
+class WidgetTicketPayControllerFactory implements FactoryInterface
 {    
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)//это тоже стандартно
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        //тут будем создавать столько переменных сколько нужно конструктору класса, который будет создавать эта фабрика.
+        
         $entityManager = $container->get('doctrine.entitymanager.orm_default');
-        $userManager = $container->get(UserManager::class);//вообще создаем переменные стандартно с использованием $container->get(имя_класса_из_окружения_подключенного_выше)
-       /* $authManager = $container->get(AuthManager::class);   */
-        // Instantiate the controller and inject dependencies
-        //возвращаем объект класса для которого создавали фабрику, передав в его конструктор выше созданные переменные
+        $userManager = $container->get(UserManager::class);
+       
         return new \User\Controller\WidgetTicketPayController($entityManager,$userManager/*,$authManager*/);
     }
 }
